3efc60bcc342e153c641bb36afcb3818244eef50
yllan <yllan@me.com>
2013-01-31 14:07:43 +0800

    Group the definitions by part-of-speech

--- src/main/scala/Moedictionary.scala
+++ src/main/scala/Moedictionary.scala
@@ -4,6 +4,17 @@ import scala.slick.driver.SQLiteDriver.simple._
 import Database.threadLocalSession
 
 object Moedictionary extends App {
+
+  def bopomofoMarkup(bpmf: String) = {
+    val isTone = Set('ˊ', 'ˇ', 'ˋ')
+
+    <span class="rt">{
+      if (isTone(bpmf.last)) bpmf.dropRight(1) else bpmf
+    }{
+      if (isTone(bpmf.last)) <span class="tone">{bpmf.last.toString}</span>
+    }</span>
+  }
+
   val db = Database.forURL(url = "jdbc:sqlite:development.unicode.sqlite3", driver = "org.sqlite.JDBC")
 
   import java.io._
@@ -34,29 +45,34 @@ object Moedictionary extends App {
     out.println("""<?xml version="1.0" encoding="UTF-8"?>""")
     out.println("""<d:dictionary xmlns="http://www.w3.org/1999/xhtml" xmlns:d="http://www.apple.com/DTDs/DictionaryService-1.0.rng">""")
     (heteronymsOf.par.filterNot(_._1.title.contains("img")).map { case (entry, heteronyms) => {
-        val xml = 
+      val xml = 
 
-        <d:entry id={entry.id.toString} d:title={entry.title}>
-        <d:index d:value={entry.title} />{
-          heteronyms.map(h => {
-            <span class="title">{entry.title}</span>
-            <span d:pr="bopomofo">{h.bopomofo.get.replaceAll("　", " ").trim}</span>
-            <div>
+      <d:entry id={entry.id.toString} d:title={entry.title}>
+      <d:index d:value={entry.title} />{
+        heteronyms.map(h => {
+          val titleWithBopomofo = entry.title zip h.bopomofo.get.split("　").drop(1)
+
+          <h1 class="title">{entry.title}<span class="bopomofo">{h.bopomofo.get}</span></h1>
+          <div>
+          {
+            definitionsOf(h).groupBy(_.partOfSpeech).map({ case (pos, ds)=> 
+              <div>{if (pos != "") <span class="part-of-speech">{pos}</span>}
               <ol>{
-                definitionsOf(h).map(d => 
+                ds.map(d =>
                   <li>
-                    <p class="definition">
-                    {(if (d.partOfSpeech != "") s"〖${d.partOfSpeech}〗" else "") + d.definition}
-                    </p>
+                    <p class="definition">{d.definition}</p>
                     { if (d.example != "") <p class="example">{d.example}</p> } 
                   </li>
                 )
               }</ol>
-            </div>
-          })
-        }</d:entry>
+              </div>
+            })
+          }
+          </div>
+        })
+      }</d:entry>
 
-        xml.toString
+      xml.toString
     }}).seq.foreach(out.println(_))
   }
   out.println("""</d:dictionary>""")
